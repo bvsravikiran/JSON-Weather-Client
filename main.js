228dfd8b50ccc846cee24a07b443e83ad98bfdd3
Ravi Kiran <vsravikiran.b@gmail.com>
2018-01-24 14:48:59 -0600

    corrected a typo

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 //Author: Venkata Satya Ravi Kiran Boggavarapu
 //Student ID: 1001541261
-//All work in this document is my original work. 
-//As far as my knowledge, I did not copied any of the content that is present in this file 
+//All work in this document is my original work.
 $(document).ready(function(){ //Faster loading. loads the script after loading the document(webpage). 
 	$("button").click(function(){ //Submit button triggers this function.
         var longitude = document.getElementById("longitude").value;//user input of longitude
 	    var latitude = document.getElementById("latitude").value;//and latitude
         var location = longitude.concat(",").concat(latitude);//join both of them with a comma(,) to form a string that represents coordinates.
-        var urlLocation = "https://api.weather.gov/points/"+location+"/stations";//the web service to retrieve the nearets weather station.
+        var urlLocation = "https://api.weather.gov/points/"+location+"/stations";//the web service to retrieve the nearest weather station.
     	$.getJSON(urlLocation, function(locationData) {	//HTTP GET request to get the JSON from the web service.
             var station = locationData.features[0].properties.stationIdentifier;//extract the station name from the JSON.
             document.getElementById("location").innerHTML = "Nearest Station: " + station;//output the station name to the HTML page.
